9be62e7518f31592d45fea5109b9342617caef20
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-04-27 20:07:49 +0000

    Hola empleado

---
+++ TP/Empleado.h
@@ -0,0 +1,20 @@
+/*
+ * Empleado.h
+ *
+ *  Created on: Apr 27, 2014
+ *      Author: ezequielgenender
+ */
+
+#ifndef EMPLEADO_H_
+#define EMPLEADO_H_
+
+namespace std {
+
+class Empleado {
+public:
+	Empleado();
+	virtual ~Empleado();
+};
+
+} /* namespace std */
+#endif /* EMPLEADO_H_ */
