0ae28d2a75f0892b501d950f8cf8dd6582142995
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-04-27 22:48:50 +0000

    constantes de archivos

---
+++ TP/constantesArchivos.h
@@ -0,0 +1,19 @@
+/*
+ * constantesArchivos.h
+ *
+ *  Created on: Apr 27, 2014
+ *      Author: ezequielgenender
+ */
+
+#ifndef CONSTANTESARCHIVOS_H_
+#define CONSTANTESARCHIVOS_H_
+
+#define ARCHIVO_CANTIDAD_EMPLEADOS "Jefe.h"
+#define DISPONIBILIDAD_EMPLEADOS 'd'
+#define ARCHIVO_CAJA "Caja.h"
+#define CAJA 'C'
+#define ARCHIVO_SURTIDORES "Empleado.h"
+#define SURTIDOR 'S'
+
+
+#endif /* CONSTANTESARCHIVOS_H_ */
