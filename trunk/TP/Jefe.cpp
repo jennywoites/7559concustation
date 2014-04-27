5e2cc3201f7a5aaf6b76d4f5de38337ff337fe2f
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-04-27 20:07:16 +0000

    https://www.youtube.com/watch?v=JRFxzFb32_g

---
+++ trunk/TP/Jefe.cpp
@@ -0,0 +1,21 @@
+/*
+ * Jefe.cpp
+ *
+ *  Created on: Apr 27, 2014
+ *      Author: ezequielgenender
+ */
+
+#include "Jefe.h"
+
+namespace std {
+
+Jefe::Jefe() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Jefe::~Jefe() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
---
+++ trunk/TP/Jefe.h
@@ -0,0 +1,20 @@
+/*
+ * Jefe.h
+ *
+ *  Created on: Apr 27, 2014
+ *      Author: ezequielgenender
+ */
+
+#ifndef JEFE_H_
+#define JEFE_H_
+
+namespace std {
+
+class Jefe {
+public:
+	Jefe();
+	virtual ~Jefe();
+};
+
+} /* namespace std */
+#endif /* JEFE_H_ */
