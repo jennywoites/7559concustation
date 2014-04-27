a917232e74157f807ae3bdad18ef6f2f17d8826e
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-04-27 20:07:49 +0000

    Hola empleado

---
+++ trunk/TP/Empleado.cpp
@@ -0,0 +1,21 @@
+/*
+ * Empleado.cpp
+ *
+ *  Created on: Apr 27, 2014
+ *      Author: ezequielgenender
+ */
+
+#include "Empleado.h"
+
+namespace std {
+
+Empleado::Empleado() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Empleado::~Empleado() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace std */
