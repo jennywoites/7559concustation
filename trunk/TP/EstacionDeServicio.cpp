007564802d9fd047a2e65ffaae0c94403d4e5211
Kadriya.Dolunay@gmail.com <Kadriya.Dolunay@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-01 19:43:51 +0000

    La Station

---
+++ trunk/TP/EstacionDeServicio.cpp
@@ -0,0 +1,44 @@
+/*
+ * EstacionDeServicio.cpp
+ *
+ *  Created on: 01/05/2014
+ *      Author: jennifer
+ */
+
+#include "EstacionDeServicio.h"
+
+void EstacionDeServicio::crearEmpleados(){
+	for(int i = 0; i<cantEmpleados; i++){
+		Empleado e ("0"+i, cantSurtidores);
+		e.atenderAutos();
+	}
+}
+
+void EstacionDeServicio::abrir(float plataInicial){
+	//abrir Log
+	//caja.abrir();
+	crearEmpleados();
+	Jefe j ("UltraAlterMaster");
+	j.atenderAutos();
+	GeneradorAutos g (mediaAutos);
+	g.generar();
+	Administrador a (100); //FIXME
+	a.mirarDinero();
+}
+
+void EstacionDeServicio::cerrar(){
+	// enviar senial a generador
+	// esperar cierre
+	// senial a admin
+	caja.cerrar();
+	// finalizar log
+}
+
+EstacionDeServicio::EstacionDeServicio(int empleados, int surtidores, int mediaGenAutos){ {
+	cantEmpleados = empleados;
+	cantSurtidores = surtidores;
+	mediaAutos = mediaGenAutos;
+}
+
+EstacionDeServicio::~EstacionDeServicio() {
+}
