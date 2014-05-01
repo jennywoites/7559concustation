1484f8792510053a8d6431fe11b56aaee86329cd
Kadriya.Dolunay@gmail.com <Kadriya.Dolunay@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-01 19:43:51 +0000

    La Station

---
+++ TP/EstacionDeServicio.h
@@ -0,0 +1,34 @@
+/*
+ * EstacionDeServicio.h
+ *
+ *  Created on: 01/05/2014
+ *      Author: jennifer
+ */
+
+#ifndef ESTACIONDESERVICIO_H_
+#define ESTACIONDESERVICIO_H_
+#include "Caja.h"
+#include "Empleado.h"
+#include "Jefe.h"
+#include "Administrador.h"
+#include "GeneradorAutos.h"
+
+using namespace std;
+
+class EstacionDeServicio {
+private:
+	int cantEmpleados;
+	int cantSurtidores;
+	int mediaAutos;
+	Caja caja;
+
+	void crearEmpleados();
+public:
+	EstacionDeServicio(int empleados, int surtidores, int mediaGenAutos);
+	virtual ~EstacionDeServicio();
+
+	void abrir(float plataInicial);
+	void cerrar();
+};
+
+#endif /* ESTACIONDESERVICIO_H_ */
