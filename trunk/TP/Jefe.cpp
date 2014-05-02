23f1f9eea56210323c1b80b5ec445c63c4a0bc65
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-02 19:46:17 +0000

    mensajitos del jefe

--- trunk/TP/Jefe.cpp
+++ trunk/TP/Jefe.cpp
@@ -9,7 +9,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include "constantesArchivos.h"
+#include "Log.h"
 
 Jefe::Jefe(std::string n, const PipeAutos& gen, const PipeAutos& aten) {
 	nombre = n;
@@ -35,7 +38,10 @@ void Jefe::atenderAutos(){
 	if (id != 0)
 		return;
 
+	Log::abrir_log();
 	Auto autito;
+
+	Log::enviarMensaje("Se ha iniciado el Proceso Jefe.");
 
 	while (leerAuto(&autito)){
 		if (!hayEmpleados()){
@@ -46,27 +52,39 @@ void Jefe::atenderAutos(){
 		tomarEmpleado();
 		enviarAutoAEmpleado(autito);
 	}
+
 	arribos.cerrar();
+	Log::enviarMensaje("Jefe cierra el pipe de lectura arribos");
 	envios.cerrar();
+	Log::enviarMensaje("Jefe cierra el pipe de escritura envios");
+	Log::enviarMensaje("Fin del proceso Jefe");
+
+	Log::cerrar_log();
 	exit(0);
 }
 
 bool Jefe::hayEmpleados(){
-	return (cantEmpleadosDisponibles.leer() > 0);
+	int cant_empleados = cantEmpleadosDisponibles.leer();
+	Log::enviarMensaje("Jefe: cantidad de empleados disponibles es " + cant_empleados);
+	return (cant_empleados > 0);
 }
 
 void Jefe::tomarEmpleado(){
+	Log::enviarMensaje("Jefe toma un empleado disponible.");
 	cantEmpleadosDisponibles.incrementar(-1);
 }
 
 void Jefe::mensajeDespachante(std::string patente){
+	Log::enviarMensaje("Jefe " + nombre + ": " + " Se ha despachado al auto de patente " + patente + " por no haber empleados libres");
 	std::string mensaje = "Jefe " + nombre + ": ";
 	mensaje += " Se ha despachado al auto de patente ";
 	mensaje += patente;
 	mensaje	+= " por no haber empleados libres\n";
 	cout << mensaje;
+	Log::enviarMensaje(mensaje);
 }
 
 bool Jefe::enviarAutoAEmpleado(const Auto& autito){
+	Log::enviarMensaje("Jefe envia un auto por el pipe hacia empleados");
 	return envios.escribirAuto(autito);
 }
