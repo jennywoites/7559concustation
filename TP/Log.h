78175e06d92613d4aaba37274376bc8d720260c9
martine9111@gmail.com <martine9111@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-02 16:16:03 +0000

    Correcciones en Caja y Admin + Funcionalidad del log

--- TP/Log.h
+++ TP/Log.h
@@ -8,12 +8,28 @@
 #ifndef LOG_H_
 #define LOG_H_
 #include <string>
+#include "MecanismoConcurrencia/Pipe.h"
 
 using namespace std;
+#define LONGITUD_MENSAJE 140
+
+typedef struct mensaje_log{
+	char mensaje[LONGITUD_MENSAJE];
+}mensaje_log_t;
+
+
+/* USO: en el proceso principal hay que crear y mandar a escribir al log.
+ * En los procesos que quieran escribir al log solamente deben hacer "abrir_log",
+ * cada vez que quieran mandar algo deben llamar a "enviarMensaje", y al terminar
+ * deben llamar a cerrar().
+ * */
 
 class Log {
 private:
 	std::string archivo;
+
+	static Pipe pipe;
+	static int valor;
 
 	static const int LOG_DEBUG = 0;
 	static const int LOG_RELEASE = 1;
@@ -24,6 +40,11 @@ public:
 	virtual ~Log();
 
 	void escribir();
+
+
+	static Pipe* abrir_log();
+	static void cerrar_log();
+	static void enviarMensaje(std::string);
 };
 
 #endif /* LOG_H_ */
