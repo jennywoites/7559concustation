d1d69cb3f7608ccfd3f63af99ebab48512668590
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-03 03:15:36 +0000

    empleado y uso del surtidor parece funcionar, siempre y cuando no este el log activado (?)

--- TP/Empleado.cpp
+++ TP/Empleado.cpp
@@ -6,52 +6,63 @@
  */
 
 #include "Empleado.h"
+#include "Log.h"
 #include "constantesArchivos.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <sstream>
 
-Empleado::Empleado(std::string name, int cantidadSurtidores, const PipeAutos& pipe) {
+Empleado::Empleado(std::string name, const PipeAutos& pipe) {
 	nombre = name;
 	cantidadAtendidos = 0;
-
-	for (int i = 0; i < cantidadSurtidores; i++){
-		MemoriaCompartida<bool> surtidor (ARCHIVO_SURTIDORES, SURTIDOR+i);
-		this->surtidores.push_back(surtidor);
-	}
 	arribos = pipe;
 }
 
 Empleado::~Empleado() {
 }
 
-void Empleado::atenderAutos(){
+void Empleado::atenderAutos(int cantidadSurtidores){
 	pid_t id = fork();
 	if (id != 0)
 		return;
 
+	Log::abrir_log();
+	Log::enviarMensaje("Se ha iniciado el Proceso Empleado "+ nombre + ".");
 	Auto autito;
 	caja.abrir();
 	disponibilidad.crear(ARCHIVO_CANTIDAD_EMPLEADOS, DISPONIBILIDAD_EMPLEADOS);
 	disponibilidad.incrementar(1);
-	while(leerAuto(&autito)){
-		cout << autito.getPatente() << endl;
-		disponibilidad.incrementar(1);
-		caja.depositar(100);
-		continue;
+	Log::enviarMensaje("Empleado: "+ nombre + " me pongo a disposicion del Jefe.");
 
+	for (int i = 0; i < cantidadSurtidores; i++){
+		MemoriaCompartida<bool> surtidor (ARCHIVO_SURTIDORES, SURTIDOR+i);
+		this->surtidores.push_back(surtidor);
+	}
+
+	while(leerAuto(&autito)){
+		Log::enviarMensaje("Empleado: " + nombre + ": Hay auto para ser atendido, patente " + string(autito.getPatente()));
 		int surtidor = tomarSurtidor();
+		stringstream num;
+		num << surtidor;
+		Log::enviarMensaje("Empleado: " + nombre + ": Logre tomar el surtidor " + num.str());
 		int litros = autito.llenar();
 		devolverSurtidor(surtidor);
+		Log::enviarMensaje("Empleado: " + nombre + ": He devuelto el surtidor " + num.str());
 		float plata = litros * PRECIO_POR_LITRO;
 		caja.depositar(plata);
+		num << plata;
+		Log::enviarMensaje("Empleado: " + nombre + ": Deposito en caja " + num.str());
 		disponibilidad.incrementar(1);
+		Log::enviarMensaje("Empleado: " + nombre + ": Me pongo en disponibilidad ");
 	}
+
 	caja.cerrar();
 	disponibilidad.liberar();
 	for (unsigned int i = 0; i < surtidores.size(); i++){
 		this->surtidores.at(i).liberar();
 	}
 
+	Log::cerrar_log();
 	exit(0);
 }
 
@@ -62,9 +73,10 @@ bool Empleado::leerAuto(Auto* autito){
 
 int Empleado::tomarSurtidor(){
 	//llamo a semaforo de surtidores: s.p()
+	//busco surtidor que NO este en uso
 	for (unsigned int i = 0; i < surtidores.size(); i++){
-		if (surtidores.at(i).leer()){ //esto rompe FIXME
-			surtidores.at(i).escribir(false);
+		if (surtidores.at(i).leer() == DESUSO){ //esto rompe FIXME
+			surtidores.at(i).escribir(true);
 			return i;
 		}
 	}
@@ -72,6 +84,6 @@ int Empleado::tomarSurtidor(){
 }
 
 void Empleado::devolverSurtidor(int surtidor){
-	surtidores.at(surtidor).escribir(true);
+	surtidores.at(surtidor).escribir(false);
 	//s.v()
 }
