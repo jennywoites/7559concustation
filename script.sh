ff7be763be2bfe9f15e97dc66f428680339048c6
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-05 19:13:05 +0000

    fix para semaforos

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IPCS_M=`ipcs -m | egrep "0x[0-9a-f]+ [0-9]+ " | grep " 644" | grep -v "dest" | cut -f2 -d" "`
-IPCS_S=`ipcs -s | egrep "0x[0-9a-f]+ [0-9]+     " | grep " 666" | grep -v "dest" | cut -f2 -d" "`
+IPCS_S=`ipcs -s | egrep "0x[0-9a-f]+ [0-9]+" | grep " 666" | grep -v "dest" | cut -f2 -d" "`
 
 for id in $IPCS_M; do
   ipcrm -m $id;
