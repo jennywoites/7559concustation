27123a9ae140d8499155b1c3fa5d67cdc159ad0d
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-05 17:23:45 +0000

    script que borra semaforos y shmem

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IPCS_M=`ipcs -m | egrep "0x[0-9a-f]+ [0-9]+ " | grep " 644" | grep -v "dest" | cut -f2 -d" "`
+IPCS_S=`ipcs -s | egrep "0x[0-9a-f]+ [0-9]+     " | grep " 666" | grep -v "dest" | cut -f2 -d" "`
+
+for id in $IPCS_M; do
+  ipcrm -m $id;
+  echo $id;
+done
+for id in $IPCS_S; do
+  ipcrm -s $id;
+  echo $id;
+done
+
+rm log.jem
