0f76e7b51bb7d76c94920306bafbac4493842e6f
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-08 02:41:36 +0000

    SigInt es miembro estructura, comenzar y finalizar

--- trunk/TP/GeneradorAutos.cpp
+++ trunk/TP/GeneradorAutos.cpp
@@ -9,34 +9,41 @@
 
 #include <unistd.h>
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <stdio.h>
 #include <stdlib.h>
 
 #include "ManejoTiempos.h"
 #include "Log.h"
-#include "MecanismoConcurrencia/SIGINT_Handler.h"
-#include "MecanismoConcurrencia/SignalHandler.h"
 
 using namespace std;
 
 GeneradorAutos::GeneradorAutos(float media, const PipeAutos& canal) {
 	this->media = media;
+	numAuto = 0;
 	envios = canal;
 }
 
 GeneradorAutos::~GeneradorAutos(){}
 
-std::string generarMensaje(int numAuto, string patente){
-	std::string mensaje = "Auto ";
+void GeneradorAutos::comenzarDia(){
+	inicializarRandom();
 
-	stringstream ss;
-	ss << numAuto;//add number to the stream
-	mensaje += ss.str();
-	mensaje += " creado, patente: ";
-	mensaje += patente;
-	return mensaje;
+	SignalHandler::getInstance()->registrarHandler(SIGINT, &sigint_handler);
+
+	Log::abrir_log();
+	Log::setEscritor("Generador de Autos");
+	Log::enviarMensaje("llamenme Ford. Roque Ford");
+	Log::setEscritor("FordMachine");
+}
+
+void GeneradorAutos::finalizarDia(){
+	Log::enviarMensaje("Llego la crisis del 30, ya no hay mas trabajo por hacer");
+	Log::enviarMensaje("Cierro el canal de autos");
+	envios.cerrar();
+	SignalHandler :: destruir ();
+	cout << "Generador deja de generar y se cierra" << endl;
+
+	Log::enviarMensaje("Fin de proceso Generador de Autos");
 }
 
 pid_t GeneradorAutos::generar(){
@@ -44,33 +51,18 @@ pid_t GeneradorAutos::generar(){
 	if (id != 0)
 		return id;
 
-	inicializarRandom();
-
-	SIGINT_Handler sigint_handler;
-	SignalHandler::getInstance()->registrarHandler(SIGINT, &sigint_handler);
-
-	int numAuto = 0;
-
-	Log::abrir_log();
-	Log::setEscritor("Generador de Autos");
-	Log::enviarMensaje("llamenme Ford. Roque Ford");
-	Log::setEscritor("FordMachine");
+	comenzarDia();
 	
 	while (sigint_handler.getGracefulQuit() == 0 ) {
 		Auto autito;
-		Log::enviarMensaje(generarMensaje(numAuto, autito.getPatente()));
+		Log::enviarMensaje("creado el auto:" + autito.getPatente() + ", numero " , numAuto);
 		envios.escribirAuto(autito);
 		int tiempo = (int) tiempoAlAzarExponencial(media);
 		usleep(tiempo);
 		numAuto++;
 	}
-	Log::enviarMensaje("Llego la crisis del 30, ya no hay mas trabajo por hacer");
-	Log::enviarMensaje("Cierro el canal de autos");
-	envios.cerrar();
-	SignalHandler :: destruir ();
-	cout << "Generador deja de generar y se cierra" << endl;
 	
-	Log::enviarMensaje("Fin de proceso Generador de Autos");
+	finalizarDia();
 	exit(0);
 }
 
