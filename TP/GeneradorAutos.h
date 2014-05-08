e9e203a272c852e2990eec0e46f21169a34f63cb
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-08 02:41:36 +0000

    SigInt es miembro estructura, comenzar y finalizar

--- TP/GeneradorAutos.h
+++ TP/GeneradorAutos.h
@@ -10,18 +10,22 @@
 
 #include "Auto.h"
 #include "MecanismoConcurrencia/PipeAutos.h"
-
-using namespace std;
+#include "MecanismoConcurrencia/SIGINT_Handler.h"
+#include "MecanismoConcurrencia/SignalHandler.h"
 
 class GeneradorAutos {
 private:
 	float media;
+	int numAuto;
 	PipeAutos envios;
+	SIGINT_Handler sigint_handler;
 
 public:
 	GeneradorAutos(float media, const PipeAutos&);
 	virtual ~GeneradorAutos();
 
+	void comenzarDia();
+	void finalizarDia();
 	pid_t generar();
 
 };
