41a2ca392406f78fedb4ff4ac1c749d10f3a1c49
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-10 23:14:04 +0000

    usted ha sido destituido de su cargo

---
+++ trunk/TP/Log2.h
@@ -0,0 +1,60 @@
+/*
+ * Log.h
+ *
+ *  Created on: 28/04/2014
+ *      Author: martin
+ */
+
+#ifndef LOG2_H_
+#define LOG2_H_
+
+#include <string>
+
+#include "MecanismoConcurrencia/Pipe.h"
+#include "constantesArchivos.h"
+
+using namespace std;
+#define LONGITUD_MENSAJE 140
+
+typedef struct mensaje_log{
+	char mensaje[LONGITUD_MENSAJE];
+}mensaje_log_t;
+
+
+/* USO: en el proceso principal hay que crear y mandar a escribir al log.
+ * En los procesos que quieran escribir al log solamente deben hacer "abrir_log",
+ * cada vez que quieran mandar algo deben llamar a "enviarMensaje", y al terminar
+ * deben llamar a cerrar().
+ * */
+
+/*
+class Log {
+private:
+	std::string archivo;
+
+	static Pipe pipe;
+	static std::string escritor;
+
+	static const int LOG_DEBUG = 0;
+	static const int LOG_RELEASE = 1;
+	static const int LOG_MODO = LOG_DEBUG;
+
+
+public:
+
+	Log(std::string path);
+	virtual ~Log();
+
+	void escribir();
+
+	static void setEscritor(std::string);
+	static Pipe* abrir_log();
+	static void cerrar_log();
+	static void enviarMensaje(std::string);
+	static void enviarMensaje(std::string, int);
+	static void enviarMensaje(std::string, float);
+
+};
+*/
+
+#endif /* LOG_H_ */
