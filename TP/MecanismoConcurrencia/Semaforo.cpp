7d5cbff8b1a9428c45c0e69d43cd0857f61e5cd8
Kadriya.Dolunay@gmail.com <Kadriya.Dolunay@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-14 20:14:15 +0000

    semaforo lanza excepciones

--- TP/MecanismoConcurrencia/Semaforo.cpp
+++ TP/MecanismoConcurrencia/Semaforo.cpp
@@ -1,5 +1,10 @@
 #include "Semaforo.h"
 
+#include <iostream>
+#include <errno.h>
+#include <string.h>
+
+
 Semaforo :: Semaforo ():id(0), valorInicial(0) {}
 
 void Semaforo :: crear ( const std::string& nombre, char letra,const int valorInicial){
@@ -7,7 +12,19 @@ void Semaforo :: crear ( const std::string& nombre, char letra,const int valorIn
 	creador.tomarLock();
 	this->valorInicial = valorInicial;
 	key_t clave = ftok (nombre.c_str(), letra);
+	if(clave == -1){
+		creador.liberarLock();
+		std::string mensaje = std::string("Error en ftok(): ") + std::string(strerror(errno));
+		throw mensaje;
+		return;
+	}
 	this->id = semget ( clave,1,0666 | IPC_CREAT );
+	if(this->id == -1){
+		creador.liberarLock();
+		std::string mensaje = std::string("Error en semget(): ") + std::string(strerror(errno));
+		throw mensaje;
+		return;
+	}
 
 	this->inicializar ();
 	creador.liberarLock();
@@ -26,6 +43,10 @@ int Semaforo :: inicializar () const {
 	semnum init;
 	init.val = this->valorInicial;
 	int resultado = semctl ( this->id,0,SETVAL,init );
+	if(resultado == -1){
+		std::string mensaje = std::string("Error en semctl(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
 	return resultado;
 }
 
@@ -38,6 +59,10 @@ int Semaforo :: wait () const {
 	operacion.sem_flg = SEM_UNDO;
 
 	int resultado = semop ( this->id,&operacion,1 );
+	if(resultado == -1){
+		std::string mensaje = std::string("Error en semop(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
 	return resultado;
 }
 
@@ -50,13 +75,26 @@ int Semaforo :: signal () const {
 	operacion.sem_flg = SEM_UNDO;
 
 	int resultado = semop ( this->id,&operacion,1 );
+	if(resultado == -1){
+		std::string mensaje = std::string("Error en semop(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
 	return resultado;
 }
 
 int Semaforo :: getVal () const {
-	return semctl(this->id, 0, GETVAL, 0);
+	int valor = semctl(this->id, 0, GETVAL, 0);
+	if(valor == -1){
+		std::string mensaje = std::string("Error en semctl(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
+	return valor;
 }
 
 void Semaforo :: eliminar () const {
-	semctl ( this->id,0,IPC_RMID );
+	int estado = semctl( this->id,0,IPC_RMID );
+	if(estado == -1){
+		std::string mensaje = std::string("Error en semctl(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
 }
