ed9e08dbce2739ea55b7587606b9628a17124f68
egenender@gmail.com <egenender@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-05-15 16:42:00 +0000

    caracter ed surtidores arranca en 0

--- trunk/TP/constantesArchivos.h
+++ trunk/TP/constantesArchivos.h
@@ -13,7 +13,7 @@
 #define ARCHIVO_CAJA "TP/Caja.h"
 #define CAJA 'C'
 #define ARCHIVO_SURTIDORES "TP/Empleado.h"
-#define SURTIDOR 'S'
+#define SURTIDOR '\0'
 #define ARCHIVO_GENERACION "TP/GeneradorAutos.h"
 #define ARCHIVO_ATENCION "TP/Auto.h"
 #define ARCHIVO_ACCESO_SURTIDORES "TP/MecanismoConcurrencia/Semaforo.h"
