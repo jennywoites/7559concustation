f038dbdddccc4d5b82b490c12474915dcabeb452
martine9111@gmail.com <martine9111@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-06-13 19:43:35 +0000

    Ahora se sincronizan el admin con los empleados para que haya prioridad para el admin. Certificado que esta funcionando. Problema: ver cuando se elimina el semaforo (deberia hacerlo el admin al cerrar)

--- TP/Caja.h
+++ TP/Caja.h
@@ -8,10 +8,14 @@
 #ifndef CAJA_H_
 #define CAJA_H_
 #include "MecanismoConcurrencia/MemoriaCompartida.h"
+#include "MecanismoConcurrencia/Semaforo.h"
 
 class Caja {
 private:
 	MemoriaCompartida<float> plata;
+	MemoriaCompartida<bool> hay_lector; // lo implementamos como bool porque solo hay un lector
+	Semaforo escritores;  //nota: con este semaforo queda al pedo el semaforo de la memoria compartida
+
 public:
 	Caja();
 	virtual ~Caja();
@@ -20,7 +24,10 @@ public:
 	float verMonto() const;
 	bool cerrar();
 	bool abrir();
-
+	void lectorQuiereUsar();
+	void lectorDeja();
+	void escritorQuiereUsar();
+	void escritorDeja();
 };
 
 #endif /* CAJA_H_ */
