41fba7f39dcf736bce02904bab74b211cd5f903c
Kadriya.Dolunay@gmail.com <Kadriya.Dolunay@gmail.com@0cf2a04e-3922-829a-126a-ece2224165b2>
2014-06-14 22:32:38 +0000

    la Cola lanza las excepciones correspondientes (igual, no estoy del todo segura de si está bien o no porque como lo escribieron devuelve un tipo tanto en el constructor como en el destructor y eso es muuuuy raro)

--- TP/MecanismoConcurrencia/Cola.h
+++ TP/MecanismoConcurrencia/Cola.h
@@ -6,6 +6,9 @@
 #include <sys/ipc.h>
 #include <stdio.h>
 #include <string>
+#include <errno.h>
+
+using namespace std;
 
 template <class T> class Cola {
 	private:
@@ -22,29 +25,46 @@ template <class T> class Cola {
 
 template <class T> Cola<T> :: Cola ( const std::string& archivo,const char letra ) {
 	this->clave = ftok ( archivo.c_str(),letra );
-	if ( this->clave == -1 )
-		perror ( "Error en ftok" );
+	if ( this->clave == -1 ){
+		std::string mensaje = std::string("Error en ftok() de Cola: ") + std::string(strerror(errno));
+		throw mensaje;
+	}
 
 	this->id = msgget ( this->clave,0777|IPC_CREAT );
-	if ( this->id == -1 )
-		perror ( "Error en msgget" );
+	if ( this->id == -1 ){
+		std::string mensaje = std::string("Error en msgget(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
+
 }
 
-template <class T> Cola<T> :: ~Cola () {
-}
+template <class T> Cola<T> :: ~Cola () {}
 
 template <class T> int Cola<T> :: destruir () const {
 	int resultado = msgctl ( this->id,IPC_RMID,NULL );
+	if(resultado == -1){
+		std::string mensaje = std::string("Error en msgctl(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
+
 	return resultado;
 }
 
 template <class T> int Cola<T> :: escribir ( const T& dato ) const {
 	int resultado = msgsnd ( this->id,static_cast<const void*>(&dato),sizeof(T)-sizeof(long),0 );
+	if(resultado == -1){
+		std::string mensaje = std::string("Error en msgsnd(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
 	return resultado;
 }
 
 template <class T> int Cola<T> :: leer ( const int tipo,T* buffer ) const {
 	int resultado = msgrcv ( this->id,static_cast<void *>(buffer),sizeof(T)-sizeof(long),tipo,0 );
+	if(resultado == -1){
+		std::string mensaje = std::string("Error en msgget(): ") + std::string(strerror(errno));
+		throw mensaje;
+	}
 	return resultado;
 }
 
